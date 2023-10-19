001936194a9daa14169d021c6106e34814104282
vishalini <vishalini@gmail.com>
2023-10-19 08:51:58 +0530

    instial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './HomePage';
+import Signin from './Signin';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <a href="#"><h1>Contextor</h1></a>
+        <ul class="list">
+          <BrowserRouter>
+            <li><Link to='/Signin'>About</Link></li>
+            <li><Link to='/Signin'>Projects</Link></li>
+            <li><Link to='/Signin'>Login</Link></li>
+          </BrowserRouter>
+        </ul>
+      </nav>
+
+      <BrowserRouter>
+        <Routes>
+
+          <Route path='/home' element={< HomePage />}></Route>
+          <Route path='/Signin' element={< Signin />}></Route>
+        </Routes>
+      </BrowserRouter>
+      <br />
     </div>
   );
 }
