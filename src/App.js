464c6a35ea5ceeba07408181d14ed2924c7d0d6d
dharunraj1982002 <dharunraj1982002@gmail.com>
2023-10-19 11:08:37 +0530

    cursor commit

--- src/App.js
+++ src/App.js
@@ -2,31 +2,131 @@ import './App.css';
 import { Link } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './HomePage';
+import React, { useEffect, useRef } from 'react'
 import Signin from './Signin';
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <a href="#"><h1>Contextor</h1></a>
-        <ul class="list">
-          <BrowserRouter>
-            <li><Link to='/Signin'>About</Link></li>
-            <li><Link to='/Signin'>Projects</Link></li>
-            <li><Link to='/Signin'>Login</Link></li>
-          </BrowserRouter>
-        </ul>
-      </nav>
-       <></>
-      <BrowserRouter>
-        <Routes>
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
 
-          <Route path='/home' element={< HomePage />}></Route>
-          <Route path='/Signin' element={< Signin />}></Route>
-        </Routes>
-      </BrowserRouter>
-      <br />
-    </div>
-  );
+
+    let mouseMoved = false;
+
+    const pointer = {
+      x: .5 * window.innerWidth,
+      y: .5 * window.innerHeight,
+    }
+    const params = {
+      pointsNumber: 40,
+      widthFactor: .3,
+      mouseThreshold: .6,
+      spring: .4,
+      friction: .5
+    };
+
+    const trail = new Array(params.pointsNumber);
+    for (let i = 0; i < params.pointsNumber; i++) {
+      trail[i] = {
+        x: pointer.x,
+        y: pointer.y,
+        dx: 0,
+        dy: 0,
+      }
+    }
+
+    window.addEventListener("click", e => {
+      updateMousePosition(e.pageX, e.pageY);
+    });
+    window.addEventListener("mousemove", e => {
+      mouseMoved = true;
+      updateMousePosition(e.pageX, e.pageY);
+    });
+    window.addEventListener("touchmove", e => {
+      mouseMoved = true;
+      updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+    });
+
+    function updateMousePosition(eX, eY) {
+      pointer.x = eX;
+      pointer.y = eY;
+    }
+
+    setupCanvas();
+    update(0);
+    window.addEventListener("resize", setupCanvas);
+
+
+    function update(t) {
+
+      // for intro motion
+      if (!mouseMoved) {
+        pointer.x = (.5 + .3 * Math.cos(.002 * t) * (Math.sin(.005 * t))) * window.innerWidth;
+        pointer.y = (.5 + .2 * (Math.cos(.005 * t)) + .1 * Math.cos(.01 * t)) * window.innerHeight;
+      }
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      trail.forEach((p, pIdx) => {
+        const prev = pIdx === 0 ? pointer : trail[pIdx - 1];
+        const spring = pIdx === 0 ? .4 * params.spring : params.spring;
+        p.dx += (prev.x - p.x) * spring;
+        p.dy += (prev.y - p.y) * spring;
+        p.dx *= params.friction;
+        p.dy *= params.friction;
+        p.x += p.dx;
+        p.y += p.dy;
+      });
+
+      ctx.beginPath();
+      ctx.moveTo(trail[0].x, trail[0].y);
+
+      for (let i = 1; i < trail.length - 1; i++) {
+        const xc = .5 * (trail[i].x + trail[i + 1].x);
+        const yc = .5 * (trail[i].y + trail[i + 1].y);
+        ctx.quadraticCurveTo(trail[i].x, trail[i].y, xc, yc);
+        ctx.lineWidth = params.widthFactor * (params.pointsNumber - i);
+        ctx.stroke();
+      }
+      ctx.lineTo(trail[trail.length - 1].x, trail[trail.length - 1].y);
+      ctx.strokeStyle = '#00fffc';
+      window.requestAnimationFrame(update);
+    }
+
+
+    function setupCanvas() {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+    }
+
+
+  }, []);
+
+  return (
+    <>
+    <BrowserRouter>
+          <Routes>
+            <Route path='/home' element={< HomePage />}></Route>
+            <Route path='/Signin' element={< Signin />}></Route>
+          </Routes>
+        </BrowserRouter>
+    <div className='App'>
+        <nav>
+          <a href="#"><h1>Contextor</h1></a>
+          <ul class="list">
+            <BrowserRouter>
+              <li><Link to='/Signin'>About</Link></li>
+              <li><Link to='/Signin'>Projects</Link></li>
+              <li><Link to='/Signin'>Login</Link></li>
+            </BrowserRouter>
+          </ul>
+        </nav>
+        
+      </div>
+    <canvas ref={canvasRef}>
+      
+      </canvas>
+      </>
+      );
 }
 
-export default App;
+      export default App;
--- src/Signin.js
+++ src/Signin.js
@@ -1,25 +1,23 @@
 import React from 'react'
 import { useState } from 'react';
 import './App.css';
-//import './Style.css';
+
 function Signin() {
+
     const [email, setEmail] = useState("");
     const [Password, setPassword] = useState("");
     const [info, setInfo] = useState('user1');
     return (
-
-        <div>
-            <div className="box1">
-                <h1 class="login">Login</h1>
-                <input type="text" className="box" placeholder="" value={email} onChange={e => setEmail(e.target.value)} />
-                <br />
-                <br />
-                <input type="Password" className="box" placeholder="" onChange={e => setPassword(e.target.value)} />
-                <br />
-                <br />
-                <button onClick={e=>setInfo(email)} >submit</button>
-                <button onClick={e=>setInfo(email)} >Create Account</button>
-            </div>
+        <div className="box1">
+            <h1 class="login">Login</h1>
+            <input type="text" className="box" placeholder="" value={email} onChange={e => setEmail(e.target.value)} />
+            <br />
+            <br />
+            <input type="Password" className="box" placeholder="" onChange={e => setPassword(e.target.value)} />
+            <br />
+            <br />
+            <button onClick={e => setInfo(email)} >submit</button>
+            <button onClick={e => setInfo(email)} >Create Account</button>
         </div>
     )
 }
