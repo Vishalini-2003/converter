e234b132af4edb02503b9ff0bc70461108a03a2d
dharunraj1982002 <dharunraj1982002@gmail.com>
2023-10-19 09:25:22 +0530

    commit1

--- src/Signin.js
+++ src/Signin.js
@@ -9,22 +9,6 @@ function Signin() {
     return (
 
         <div>
-            
-<span style={{'-i': 0}} />
-        <span style={{'-i': 1}} />
-        <span style={{'-i': 2}} />
-        <span style={{'-i': 3}} />
-        <span style={{'-i': 4}} />
-        <span style={{'-i': 5}} />
-        <span style={{'-i': 6}} />
-        <span style={{'-i': 7}} />
-        <span style={{'-i': 8}} />
-        <span style={{'-i': 9}} />
-        <span style={{'-i': 10}} />
-        <span style={{-i: 11}} />
-        <span style={{-i: 12}} />
-        <span style={{-i: 13}} />
-        <span style={{-i: 14}} />
             <div className="box1">
                 <h1 class="login">Login</h1>
                 <input type="text" className="box" placeholder="" value={email} onChange={e => setEmail(e.target.value)} />
@@ -33,8 +17,8 @@ function Signin() {
                 <input type="Password" className="box" placeholder="" onChange={e => setPassword(e.target.value)} />
                 <br />
                 <br />
-                <button onClick={e => setInfo(email)} >submit</button>
-                <button onClick={e => setInfo(email)} >Create Account</button>
+                <button onClick={e=>setInfo(email)} >submit</button>
+                <button onClick={e=>setInfo(email)} >Create Account</button>
             </div>
         </div>
     )
